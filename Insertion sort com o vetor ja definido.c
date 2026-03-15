a1ea7fa82b2470f5a534531e71033bc0c06805fc
Jeanne Espíndola <jeanneespindolapereira@gmail.com>
2026-03-15 15:19:38 -0300

    Update Insertion sort com o vetor ja definido.c

--- Insertion sort com o vetor ja definido.c
+++ Insertion sort com o vetor ja definido.c
@@ -7,7 +7,7 @@ int main() {
   int i, anterior, atual //i e o indice, o anterior é o numero anterior do vetor e atual é o valor que esta no i
 
   for(i = 1; i <= TAM; i += 1) { //i se inicia no 1 pois começamos a analisar a partir do 2o valor. O primeiro é assumido como ordenado inicialmente
-    atual = numeros[i] //Estamos acessando o numero individualmente do indice dentro do for e guardando o valor no numero atual.
+    atual = numeros[i] //Estamos acessando o indice dentro do for e guardando o valor no numero atual.
     anterior = i - 1; //Anterior esta indicado por i - 1, pois ele se trata do valor anterior do indice.
     while (anterior >= 0 && numeros[anterior] > atual) { //enquanto o anterior nao sair do vetor (n for menor q 0) e o numero anterior for maior q o atual
       numeros[anterior + 1] = numeros[anterior] //o numero maior vai para a direita
