370dbf8b8dd7383264ed8ac6937aea1975ab76c0
Jeanne Espíndola <jeanneespindolapereira@gmail.com>
2026-03-15 17:24:15 -0300

    Insertion sort com um vetor ja pronto

--- Insertion sort com o vetor ja definido.c
+++ Insertion sort com o vetor ja definido.c
@@ -1,22 +1,30 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
-#define TAM 4
-//Questao com insertion sort a partir de um vetor ja pronto
+#define TAM 7
+
+void ordenar_precos(double precos[], int tamanho){ //definicao dos parametros e funcao
+  int i, anterior;
+  double atual;
+
+for(i = 0; i < tamanho; i++) {
+  atual = precos[i]; //definicao do indice de precos ser igual a variavel atual
+  anterior = i - 1;} //anterior para voltar ao indice 0 para comparacao
+  while(anterior >= 0 && precos[anterior] > atual) { //anterior tem q ser maior q 0 ou igual para nao fugir dos indices e o preco anterior tem que ser maior q o atual para ser colocado na esquerda.
+    precos[anterior + 1] = precos[anterior] //o anterior mais um é a cadeira da direita e o preco anterior normal é a esquerda, voce meio que pega o valor que ta na esquerda e bota pra direita se ele estiver de acordo com o while.
+    anterior = anterior - 1} //para garantir que ele sempre diminua para comparacao 
+  precos[anterior + 1] = atual; } //definir o indice seguinte como atual fora do while, pois so vai ser executado depois que todas estiverem ordenadas
+
 int main() {
-  int numeros [TAM] = {3, 4, 1, 2}; //Vetor ja criado sem pedir pro usuario.
-  int i, anterior, atual; //i e o indice, o anterior é o numero anterior do vetor e atual é o valor que esta no i
+  double precos[7] = {45.50, 12.90, 89.00, 5.50, 34.00, 21.00, 67.00};
+  int i;
 
-  for(i = 1; i <= TAM; i += 1) { //i se inicia no 1 pois começamos a analisar a partir do 2o valor. O primeiro é assumido como ordenado inicialmente
-    atual = numeros[i]; //Estamos acessando o indice dentro do for e guardando o valor no numero atual.
-    anterior = i - 1; //Anterior esta indicado por i - 1, pois ele se trata do valor anterior do indice.
-    while (anterior >= 0 && numeros[anterior] > atual) { //enquanto o anterior nao sair do vetor (n for menor q 0) e o numero anterior for maior q o atual
-      numeros[anterior + 1] = numeros[anterior]; //o numero maior vai para a direita
-    numeros[anterior + 1] = atual; } }
+  ordenar_precos(precos, TAM);
 
-printf("Números ordenados: ");
-    for (i = 0; i < TAMANHO; i++) {
-        printf("%d ", numeros[i]);
-    }
+  for(i = 0; i < TAM; i++) {
+    printf("{%.2f}", precos[i]);}
 
-    return 0;
-}
+return 0; }
+
+  
+
+      
