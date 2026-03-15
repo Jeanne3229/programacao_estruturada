0f966d143dc53b30e7de4b77cb223963191d41f3
Jeanne Espíndola <jeanneespindolapereira@gmail.com>
2026-03-15 15:13:34 -0300

    Insertion sort com um vetor ja pronto

---
+++ Insertion sort com o vetor ja definido.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#define TAM 4
+//Questao com insertion sort a partir de um vetor ja pronto
+int main() {
+  int numeros [TAM] = {3, 4, 1, 2} //Vetor ja criado sem pedir pro usuario.
+  int i, anterior, atual //i e o indice, o anterior é o numero anterior do vetor e atual é o valor que esta no i
+
+  for(i = 1; i <= TAM; i += 1) { //i se inicia no 1 pois começamos a analisar a partir do 2o valor. O primeiro é assumido como ordenado inicialmente
+    atual = numeros[i] //Estamos acessando o numero individualmente do indice dentro do for e guardando o valor no numero atual.
+    anterior = i - 1; //Anterior esta indicado por i - 1, pois ele se trata do valor anterior do indice.
+    while (anterior >= 0 && numeros[anterior] > atual) { //enquanto o anterior nao sair do vetor (n for menor q 0) e o numero anterior for maior q o atual
+      numeros[anterior + 1] = numeros[anterior] //o numero maior vai para a direita
+    numeros[anterior + 1] = atual;
+
+printf("Números ordenados: ");
+    for (i = 0; i < TAMANHO; i++) {
+        printf("%d ", numeros[i]);
+    }
+
+    return 0;
+}
